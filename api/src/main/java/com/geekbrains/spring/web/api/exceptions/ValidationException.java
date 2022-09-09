e76bdfd3ca3ed5fe689999036b3c9a350f6eb862
bafkit <konyrevpv@mail.ru>
2022-09-09 09:56:57 +0300

    Adding cart tests Transfer dto and exceptions

--- api/src/main/java/com/geekbrains/spring/web/api/exceptions/ValidationException.java
+++ api/src/main/java/com/geekbrains/spring/web/api/exceptions/ValidationException.java
@@ -1,16 +1,21 @@
 package com.geekbrains.spring.web.api.exceptions;
 
-import lombok.Data;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Data
 public class ValidationException extends RuntimeException {
     private List<String> errorFieldsMessages;
 
     public ValidationException(List<String> errorFieldsMessages) {
         super(errorFieldsMessages.stream().collect(Collectors.joining(", ")));
+        this.errorFieldsMessages = errorFieldsMessages;
+    }
+
+    public List<String> getErrorFieldsMessages() {
+        return errorFieldsMessages;
+    }
+
+    public void setErrorFieldsMessages(List<String> errorFieldsMessages) {
         this.errorFieldsMessages = errorFieldsMessages;
     }
 }
---
+++ cart-service/src/test/java/com/geekbrains/spring/cart/tests/CartTest.java
@@ -0,0 +1,78 @@
+package com.geekbrains.spring.cart.tests;
+
+import com.geekbrains.spring.cart.services.CartService;
+import com.geekbrains.spring.web.api.dto.ProductDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+public class CartTest {
+
+    @Autowired
+    private CartService cartService;
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    public void initCart() {
+        cartService.clear("test_cart2");
+    }
+
+    @Test
+    public void addToCartTest() {
+        ProductDto productDtoMilk = new ProductDto();
+        productDtoMilk.setId(10L);
+        productDtoMilk.setTitle("Milk");
+        productDtoMilk.setPrice(100);
+
+        Mockito.doReturn(productDtoMilk).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 10L, ProductDto.class);
+
+        cartService.addProductByIdToCart(10L, "test_cart2");
+        cartService.addProductByIdToCart(10L, "test_cart2");
+        cartService.addProductByIdToCart(10L, "test_cart2");
+
+        Mockito.verify(restTemplate, Mockito.times(1)).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 10L, ProductDto.class);
+        Assertions.assertEquals(1, cartService.getCurrentCart("test_cart2").getItems().size());
+    }
+
+
+    @Test
+    public void deleteFromCartTest() {
+        ProductDto productDtoMilk = new ProductDto();
+        productDtoMilk.setId(10L);
+        productDtoMilk.setTitle("Milk");
+        productDtoMilk.setPrice(100);
+
+        ProductDto productDtoCheese = new ProductDto();
+        productDtoCheese.setId(11L);
+        productDtoCheese.setTitle("Cheese");
+        productDtoCheese.setPrice(170);
+
+        ProductDto productDtoBread = new ProductDto();
+        productDtoBread.setId(12L);
+        productDtoBread.setTitle("Bread");
+        productDtoBread.setPrice(20);
+
+        Mockito.doReturn(productDtoMilk).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 10L, ProductDto.class);
+        Mockito.doReturn(productDtoCheese).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 11L, ProductDto.class);
+        Mockito.doReturn(productDtoBread).when(restTemplate).getForObject("http://localhost:8189/web-market-core/api/v1/products/" + 12L, ProductDto.class);
+
+        cartService.addProductByIdToCart(10L, "test_cart2");
+        cartService.addProductByIdToCart(11L, "test_cart2");
+        cartService.addProductByIdToCart(12L, "test_cart2");
+
+        Assertions.assertEquals(3, cartService.getCurrentCart("test_cart2").getItems().size());
+
+        cartService.deleteFromCart(10L, "test_cart2");
+        cartService.deleteFromCart(12L, "test_cart2");
+
+        Assertions.assertEquals(1, cartService.getCurrentCart("test_cart2").getItems().size());
+    }
+}
